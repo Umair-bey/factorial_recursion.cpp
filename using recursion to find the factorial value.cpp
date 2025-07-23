b956f34f3512644a36d6ed05e91a5efde25db292
Umair-bey <umair2025khan.in@gmail.com>
2025-07-23 16:46:48 +0530

    Add files via upload

---
+++ using recursion to find the factorial value.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int nProduct(int p){
+  if (p == 0)
+    return 1;
+  int product = p * nProduct(p-1);
+  return product;
+
+}
+int main( ){
+int n;
+cout<<"Enter the value of n: "; 
+  cin>>n;
+  cout<<"the factorial of n is: ";
+  cout<<nProduct(n);
+  return 0;
+}
